d3c4735a77599fcc07ca1faac9fee0529cdf60be
Kabir Khan <kkhan@redhat.com>
2023-03-01 17:16:02 +0000

    Copy QS from old location

---
+++ src/main/java/org/jboss/as/quickstarts/remote/mdb/RemoteBrokerApp.java
@@ -0,0 +1,11 @@
+package org.jboss.as.quickstarts.remote.mdb;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author <a href="mailto:kabir.khan@jboss.com">Kabir Khan</a>
+ */
+@ApplicationPath("")
+public class RemoteBrokerApp extends Application {
+}
